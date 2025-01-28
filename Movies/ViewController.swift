acb92efc7dfcc41299d81f8876461430140eb482
OlegBazilevich <bazylevich1209@icloud.com>
2025-01-28 17:26:23 +0200

    Initial Commit

---
+++ Movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Oleg Bazilevich on 28.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
